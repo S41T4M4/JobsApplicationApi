d6729c33ebef7e0f5de22a65922107d9a03b726d
S41T4M4 <ibraim99.5@gmail.com>
2024-08-05 14:07:40 -0300

    Autenticação

--- Controllers/v1/AuthController.cs
+++ Controllers/v1/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using JobApplication.Domain.Models;
+﻿using Microsoft.Extensions.Logging;
+using JobApplication.Domain.Models;
 using JobApplication.Services;
 using JobApplication.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -11,30 +12,37 @@ namespace JobApplication.Controllers
     {
         private readonly IJobRepository _jobRepository;
         private readonly TokenService _tokenService;
+        private readonly ILogger<AuthController> _logger;
 
-        public AuthController(IJobRepository jobRepository, TokenService tokenService)
+        public AuthController(IJobRepository jobRepository, TokenService tokenService, ILogger<AuthController> logger)
         {
             _jobRepository = jobRepository;
             _tokenService = tokenService;
+            _logger = logger;
         }
 
         [HttpPost("login")]
         public IActionResult Auth([FromBody] LoginViewModel loginViewModel)
         {
-            if (loginViewModel == null || string.IsNullOrEmpty(loginViewModel.Email) || string.IsNullOrEmpty(loginViewModel.Senha))
+            if (loginViewModel == null || string.IsNullOrEmpty(loginViewModel.Email) || string.IsNullOrEmpty(loginViewModel.Senha) || string.IsNullOrEmpty(loginViewModel.Perfil))
             {
+                _logger.LogWarning("Credenciais inválidas fornecidas.");
                 return BadRequest("Credenciais inválidas.");
             }
 
             var user = _jobRepository.GetUsuariosByEmail(loginViewModel.Email);
-            if (user == null || user.senha != loginViewModel.Senha) 
+
+            if (user == null || user.senha != loginViewModel.Senha || user.perfil != loginViewModel.Perfil)
             {
+                _logger.LogWarning("Tentativa de login falhou para o email: {Email}", loginViewModel.Email);
                 return Unauthorized("Email ou senha inválidos.");
             }
 
+            // Gera um token para o usuário autenticado.
             var token = _tokenService.GenerateToken(user);
+            _logger.LogInformation("Usuário autenticado com sucesso: {Email}", user.email);
 
-            return Ok(token);
+            return Ok(new { Message = token, perfil = user.perfil });
         }
     }
 }
