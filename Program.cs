55407bf791eeada44ad570b79fa09f4cdb125988
S41T4M4 <ibraim99.5@gmail.com>
2024-09-10 12:52:17 -0300

    Editado o program.cs

--- Program.cs
+++ Program.cs
@@ -77,11 +77,12 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: "MyPolicy",
         policy =>
         {
-            policy.WithOrigins("http://localhost:8080", "http://localhost:4200")
-            .AllowAnyHeader()
-            .AllowAnyMethod();
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
         });
 });
+
 
 var key = Encoding.ASCII.GetBytes(JobApplication.Key.Secret);
 
